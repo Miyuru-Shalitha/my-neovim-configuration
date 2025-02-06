0f2a94534df1aefd2cdbf16147374673e130e240
Miyuru-Shalitha <miyurushalitha@gmail.com>
2025-02-06 08:19:31 +0530

    Initial commit.

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = false
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
