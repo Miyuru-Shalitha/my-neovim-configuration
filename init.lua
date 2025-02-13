dbe07f79000c1dc35a446ad6936b422aca7f9e9e
Miyuru-Shalitha <miyurushalitha@gmail.com>
2025-02-14 00:33:01 +0530

    Add Lsp, formatting, fuzy finding, completions

--- init.lua
+++ init.lua
@@ -1,9 +1,49 @@
-require("config.lazy")
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.wrap = false
+vim.opt.colorcolumn = "81"
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
+vim.opt.signcolumn = 'yes'
+vim.opt.list = true
+vim.opt.listchars = { tab = '>> ', trail = '.', nbsp = '_' }
+vim.opt.cursorline = true
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
+vim.keymap.set("n", "W", vim.diagnostic.open_float, { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking.",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = ""
+    }
+})
+
+require("config.lazy")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+        },
+        config = function()
+            local telescope = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>fh", telescope.help_tags)
+            vim.keymap.set("n", "<leader>ff", telescope.find_files)
+            vim.keymap.set("n", "<leader>en", function()
+                telescope.find_files {
+                    cwd = vim.fn.stdpath("config")
+                }
+            end
+            )
+        end
+    }
+}
