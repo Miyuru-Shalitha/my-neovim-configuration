c8b91d0831c440439f05007ff9ab28669600b4ff
Miyuru-Shalitha <miyurushalitha@gmail.com>
2025-03-12 01:55:42 +0530

    Add none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,52 +1,53 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "saghen/blink.cmp",
-            {
-                "folke/lazydev.nvim",
-                ft = "lua", -- only load on lua files
-                opts = {
-                    library = {
-                        -- See the configuration section for more details
-                        -- Load luvit types when the `vim.uv` word is found
-                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                    },
-                },
-            }
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ts_ls", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "saghen/blink.cmp",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
-        config = function()
-            local capabilities = require('blink.cmp').get_lsp_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
+      },
+    },
+    config = function()
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
 
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(event)
-                    local buf = event.buf
-                    local opts = { buffer = buf, noremap = true, silent = true }
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(event)
+          local buf = event.buf
+          local opts = { buffer = buf, noremap = true, silent = true }
 
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
-                end
-            })
-        end
-    }
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          --vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
+        end,
+      })
+    end,
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,37 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        -- or                              , branch = '0.1.x',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    -- or                              , branch = '0.1.x',
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      },
+    },
+    config = function()
+      local telescope = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>fh", telescope.help_tags)
+      vim.keymap.set("n", "<leader>ff", telescope.find_files)
+      vim.keymap.set("n", "<leader>en", function()
+        telescope.find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
+      end)
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
         },
-        config = function()
-            local telescope = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>fh", telescope.help_tags)
-            vim.keymap.set("n", "<leader>ff", telescope.find_files)
-            vim.keymap.set("n", "<leader>en", function()
-                telescope.find_files {
-                    cwd = vim.fn.stdpath("config")
-                }
-            end
-            )
-        end
-    }
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
